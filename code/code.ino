7ee388b70fcefc1a27d07ff23061062bbf4c2cff
Thanh <lehoangminhthanh43@gmail.com>
2023-09-11 19:17:43 +0700

    auto ket noi nhung chua xu ly save mat khau

--- code/code.ino
+++ code/code.ino
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <WiFi.h>
 #include <FirebaseESP32.h>
 #include <WiFiManager.h>
@@ -5,35 +6,35 @@
 // Firebase ESP32 credentials
 #define FIREBASE_HOST "datn-efb05-default-rtdb.asia-southeast1.firebasedatabase.app"
 #define FIREBASE_AUTH "cs1xOkrXUMAtoQmaUhnDgmSpjUnnPgDolcC8VSHm"
-#define TRIGGER_PIN 0
 
-unsigned int timeout = 120;
+uint32_t timeout = 120;
+uint32_t count = 0;
 
 // FirebaseESP32 object
 FirebaseData firebaseData;
+WiFiManager wm;  
 
-void connectToWifi(void) {
+void connectToWifi(void) 
+{
   // Enter WiFi credentials
   const char* ssid = "NT3";
   const char* password = "Thanh@@026099566";
-
+  
   // Connect to WiFi
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) 
+  {
     delay(500);
     Serial.print(".");
     // is configuration portal requested?
-    if ( digitalRead(TRIGGER_PIN) == LOW) {
-      WiFiManager wm;    
-
+    if (count == 10) 
+    {      
       //reset settings - for testing
-      wm.resetSettings();
-    
+      wm.resetSettings();    
       // set configportal timeout
       wm.setConfigPortalTimeout(timeout);
-
       if (!wm.startConfigPortal("KHOACUA")) {
         Serial.println("failed to connect and hit timeout");
         delay(3000);
@@ -42,30 +43,23 @@ void connectToWifi(void) {
         delay(5000);
       }
     }
+    count++;
   }
-
   Serial.println("\nConnected to WiFi");
 }
 
-void resetWifi(void)
+void setup(void) 
 {
-  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP  
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("\n Starting");
-  pinMode(TRIGGER_PIN, INPUT_PULLUP);
-}
-
-void setup() {
-  Serial.begin(9600);
-  resetWifi();
+  WiFi.mode(WIFI_STA);
   // Connect to WiFi
   connectToWifi();
   // Initialize FirebaseESP32
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
-void loop() {
+void loop(void) 
+{
   // Send "Hello" to Firebase Realtime Database
   Firebase.setString(firebaseData, "/message", "Hello");
 
