9f601f2cd94b31d7611ed9f1f3253c9d36b705b9
Thanh <lehoangminhthanh43@gmail.com>
2023-09-24 09:38:52 +0700

    Android login code reported on Wednesday, September 20

---
+++ codeAndroid/app/src/main/java/com/example/code_android_datn/Khu_tro_1/ViewPagerKhu_tro_1.java
@@ -0,0 +1,4 @@
+package com.example.code_android_datn.Khu_tro_1;
+
+public class ViewPagerKhu_tro_1 {
+}
---
+++ codeAndroid/app/src/main/java/com/example/code_android_datn/Khu_tro_1/fragment_khu_tro_quan_ly.java
@@ -0,0 +1,8 @@
+package com.example.code_android_datn.Khu_tro_1;
+
+import androidx.fragment.app.Fragment;
+
+public class fragment_khu_tro_quan_ly extends Fragment
+{
+
+}
