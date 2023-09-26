e9983520215163ce71e274bd1bf78b3bedea7d53
thanhlehoangminh <117965669+thanhlehoangminh@users.noreply.github.com>
2023-09-26 14:42:43 +0700

    Delete codeAndroid directory

--- codeAndroid/app/src/main/java/com/example/code_android_datn/Khu_tro_1/ViewPagerKhu_tro_1.java
+++
@@ -1,4 +0,0 @@
-package com.example.code_android_datn.Khu_tro_1;
-
-public class ViewPagerKhu_tro_1 {
-}
--- codeAndroid/app/src/main/java/com/example/code_android_datn/Khu_tro_1/fragment_khu_tro_quan_ly.java
+++
@@ -1,8 +0,0 @@
-package com.example.code_android_datn.Khu_tro_1;
-
-import androidx.fragment.app.Fragment;
-
-public class fragment_khu_tro_quan_ly extends Fragment
-{
-
-}
