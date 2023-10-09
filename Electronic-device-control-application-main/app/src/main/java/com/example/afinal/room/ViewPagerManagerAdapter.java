63f87f01123af31ab94fda5bd01f99d06899bccc
PC <https://github.com/duyquan27/Smart_Home_Mobile>
2023-10-09 10:41:53 +0700

    new

--- Electronic-device-control-application-main/app/src/main/java/com/example/afinal/room/ViewPagerManagerAdapter.java
+++ Electronic-device-control-application-main/app/src/main/java/com/example/afinal/room/ViewPagerManagerAdapter.java
@@ -4,44 +4,39 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentPagerAdapter;
 
-public class ViewPagerManagerAdapter extends FragmentStatePagerAdapter {
+import java.util.ArrayList;
+import java.util.List;
 
-    public ViewPagerManagerAdapter(@NonNull FragmentManager fm, int behavior) {
-        super(fm, behavior);
+public class ViewPagerManagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> fragments = new ArrayList<>();
+    private final List<String> fragmentTitle = new ArrayList<>();
+
+    public ViewPagerManagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+
+    public void add(Fragment fragment, String title) {
+        fragments.add(fragment);
+        fragmentTitle.add(title);
     }
 
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        switch (position){
-            case 0:
-                return new memberFragment();
-            case 1:
-                return new overviewFragment();
-            default:
-                return new memberFragment();
-        }
+        return fragments.get(position);
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return fragments.size();
     }
 
     @Nullable
     @Override
     public CharSequence getPageTitle(int position) {
-        String title = "";
-        switch (position) {
-            case 0:
-                title = "Quản lý";
-                break;
-            case 1:
-                title = "Tổng quan";
-                break;
-        }
-        return title;
+        return fragmentTitle.get(position);
     }
 }
